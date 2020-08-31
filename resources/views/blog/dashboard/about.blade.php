d2c992e8390c3deeb35f278a81244e2241643def
EmmaSibo <sibo@2019gitlab>
2020-08-31 17:42:08 +0200

    Add CMS in bolder text

--- resources/views/blog/dashboard/about.blade.php
+++ resources/views/blog/dashboard/about.blade.php
@@ -179,15 +179,15 @@
                                         </div>
                                     </li>
                                     <li class="mb-2">
-                                        <div class="resume-skill-name">CMS:WordPress</div>
+                                        <div class="resume-skill-name"><strong>CMS:</strong>WordPress</div>
                                         <div class="progress resume-progress">
-                                            <div class="progress-bar theme-progress-bar-dark" role="progressbar" style="width: 82%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                                            <div class="progress-bar theme-progress-bar-dark" role="progressbar" style="width: 92%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
                                         </div>
                                     </li>
                                     <li class="mb-2">
-                                        <div class="resume-skill-name">CMS:Drupal</div>
+                                        <div class="resume-skill-name"><strong>CMS:</strong>Drupal</div>
                                         <div class="progress resume-progress">
-                                            <div class="progress-bar theme-progress-bar-dark" role="progressbar" style="width: 82%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                                            <div class="progress-bar theme-progress-bar-dark" role="progressbar" style="width: 90%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
                                         </div>
                                     </li>
                                 </ul>
